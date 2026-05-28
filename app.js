45b68a8c5feb50d4e9e78bef6e33ad9dfaf0b35a
ilyoshs <25005771@myrp.edu.sg>
2026-05-28 12:10:56 +0800

    Add welcome page and update routing in app.js

--- app.js
+++ app.js
@@ -7,20 +7,33 @@ const app = express();
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+// Serve static assets (images, custom CSS stylesheets) from public directory
+app.use(express.static('public'));
+
 // Middleware to parse request bodies
 app.use(express.urlencoded({ extended: true }));
 
 // Declare any necessary variables or in-memory data structures here
-
+let studyLogs = [];
 
 // TASK: Define appropriate routes below
 // ---------------------------------------------------
+
+//Define a route to render the welcome page 
+app.get('/', (req, res) => {
+    res.render('welcome');
+});
 
 //Define a route to render the index page
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+//Main Dashboard Route - This is where the "Enter" button takes you
+app.get('/dashboard', (req, res) => {
+    // We send the user to index.ejs AND give the page access to our studyLogs array
+    res.render('index', { logs: studyLogs });
+});
 // ---------------------------------------------------
 
 // Start the server
